ab5e2477422cd5a0d4d5fc7f860e9f8cc343f64f
Jesse <jsabel@academic.rrc.ca>
2022-03-30 09:06:13 -0500

    Added the models to activeadmin dashboard

---
+++ app/admin/platforms.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Platform do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+   permit_params :platform_name
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:platform_name]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+end
