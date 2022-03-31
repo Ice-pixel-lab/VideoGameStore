d80ed3430e4daa471cb1f4db63973db6ca0ba5a7
Jesse <jsabel@academic.rrc.ca>
2022-03-31 09:51:51 -0500

    Created the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'game/index'
+  get 'game/show'
+  get "welcome/index"
+  root "welcome#index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
