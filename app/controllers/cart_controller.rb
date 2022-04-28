9ec2921dc93edd4b85eda92a60b7768a300f3469
Jesse <jsabel@academic.rrc.ca>
2022-04-27 21:20:16 -0500

    Fixed all rubocop issues

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,13 +2,11 @@ class CartController < ApplicationController
   # POST /cart
   # data is sent as form data (params)
   def create
-    logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
     return if session[:shopping_cart].include?(id)
 
     session[:shopping_cart] << id # push id onto the end of the cart array
     game = Game.find(id)
-    redirect_to root_path
     flash[:notice] = "➕ #{game.game_name} added to cart!"
   end
 end
@@ -19,5 +17,4 @@ def destroy
   session[:shopping_cart].delete(id)
   game = Game.find(id)
   flash[:notice] = "➖ #{game.game_name} removed from cart!"
-  redirect_to root_path
 end
